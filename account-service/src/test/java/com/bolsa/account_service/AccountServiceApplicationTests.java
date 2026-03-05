342bbf15061899b84d8a50d279abc55b6a5366ae
Jou1196 <jariasoto96@gmail.com>
2026-03-04 23:20:12 -0500

    add new services and controllers

---
+++ account-service/src/test/java/com/bolsa/account_service/AccountServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bolsa.account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
+@SpringBootTest
+@Disabled("Disabled")
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ movement-service/src/test/java/com/bolsa/movement_service/MovementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsa.movement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
